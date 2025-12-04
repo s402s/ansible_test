bfd6aee9d9f92653c50ac7d77d3839b6c700e5e5
s402s <156640809+s402s@users.noreply.github.com>
2025-12-04 17:04:49 +0500

    Update agrg-newserver.sh

--- agrg-newserver.sh
+++ agrg-newserver.sh
@@ -6,5 +6,5 @@ echo -n "Enter server ip address and press [ENTER]: "
 read name
 echo -n "Enter ssh user name [sysadm1n] press [ENTER]: "
 read user
-user=${user:-sysadm1n}
+user=sysadm1n
 ansible-playbook -b new.yml -k -K -u $user -e ansible_become_method=su --extra-vars "target=$name user=$user"
