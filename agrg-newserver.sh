82793cb54ef9d77a2b585b7e04fce0c5381cf00c
s402s <156640809+s402s@users.noreply.github.com>
2025-12-04 17:06:55 +0500

    Update agrg-newserver.sh

--- agrg-newserver.sh
+++ agrg-newserver.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 echo "Hello, "$USER".  This script will install bootstrap package for ansible in new server."
-
-echo -n "Enter server ip address and press [ENTER]: "
+echo -n "Enter server ip address and press [ENTER]: " 10.111.2.188
 read name
-echo -n "Enter ssh user name [sysadm1n] press [ENTER]: "
+echo -n "Enter ssh user name [sysadm1n] press [ENTER]: " sysadm1n
 read user
 user=sysadm1n
 ansible-playbook -b new.yml -k -K -u $user -e ansible_become_method=su --extra-vars "target=$name user=$user"
